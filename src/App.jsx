37e7996d5982d36b2dea6c499c3749906b0c1925
Rahul Kumar <rahulkumar9199270356@gmail.com>
2026-03-05 13:56:58 +0530

    implemen the routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const App = () => {
+import Header from "./components/Header";
+import ProductList from "./components/ProductList";
+import ProductDetail from "./components/ProductDetail";
+import Cart from "./components/Cart";
+import Checkout from "./components/Checkout";
+import NotFound from "./components/NotFound";
+
+function App() {
   return (
-    <div >App</div>
-  )
+    <BrowserRouter>
+      <Header />
+
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
